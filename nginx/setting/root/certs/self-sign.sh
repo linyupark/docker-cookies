558fc3105941be227f2f10b389a319928f778389
林宇 <jones.lin@megalinkps.cn>
2022-09-02 14:09:11 +0800

    feat(自签泛域名成功)

--- nginx/setting/root/certs/self-sign.sh
+++ nginx/setting/root/certs/self-sign.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-readonly DOMAIN=megalink.test
+readonly DOMAIN=*.megalink.test
 
 mkdir -p ./${DOMAIN}
 echo "[dn]\nCN=${DOMAIN}\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:${DOMAIN}\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth" > ./config
 
-openssl req -x509 -out $DOMAIN/${DOMAIN}.crt -keyout $DOMAIN/${DOMAIN}.key -newkey rsa:2048 -nodes -sha256 -subj "/CN=${DOMAIN}" -extensions EXT -config ./config
+openssl req -x509 -days 365 -out $DOMAIN/${DOMAIN}.crt -keyout $DOMAIN/${DOMAIN}.key -newkey rsa:2048 -nodes -sha256 -subj "/CN=${DOMAIN}" -extensions EXT -config ./config
 
 rm ./config
 
